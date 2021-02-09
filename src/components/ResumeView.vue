90117f4fc238425d75e3e341f1982c7af7440e39
Рыбников Андрей Анатольевич <43681241+egolegegit4226@users.noreply.github.com>
2021-02-09 22:07:30 +0700

    studied on the example of decomposition into components

---
+++ src/components/ResumeView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="card card-w70">
+    <template v-if="blocks.length">
+      <component
+        v-for="block in blocks"
+        :key="block.id"
+        :is="'resume-' + block.type"
+        v-bind="{value: block.value}"
+      ></component>
+    </template>
+    <h3 v-else>Добавьте первый блок, чтобы увидеть результат</h3>
+  </div>
+</template>
+
+<script>
+import ResumeAvatar from './resumeItem/ResumeAvatar'
+import ResumeSubtitle from './resumeItem/ResumeSubtitle'
+import ResumeText from './resumeItem/ResumeText'
+import ResumeTitle from './resumeItem/ResumeTitle'
+
+export default {
+  props: ['blocks'],
+  components: {
+    ResumeTitle,
+    ResumeText,
+    ResumeSubtitle,
+    ResumeAvatar
+  }
+}
+</script>

1ab791ab404d35392d3a38207e0641429ecbacf6
Рыбников Андрей Анатольевич <43681241+egolegegit4226@users.noreply.github.com>
2021-02-10 22:36:33 +0700

    response error async fetch. close block error comments

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,12 @@
   </div>
   <div class="container">
     <AppLoader v-if="loading" />
-    <ResumeComments
-        v-else
-        :comments="comments"
-        @load-comments="loadComments"
+    <ErrorComments
+      v-else-if="commentsError"
+      :textError1="textError"
+      @close-CommentsError="closeCommentsError"
     />
+    <ResumeComments v-else :comments="comments" @load-comments="loadComments" />
   </div>
 </template>
 
@@ -18,28 +19,56 @@ import ResumeForm from './components/ResumeForm'
 import ResumeView from './components/ResumeView'
 import ResumeComments from './components/ResumeComments'
 import AppLoader from './components/AppLoader'
+import ErrorComments from './components/ErrorComments'
 
 export default {
   data () {
     return {
       blocks: [],
       comments: [],
-      loading: false
+      loading: false,
+      commentsError: false,
+      textError: []
     }
   },
   methods: {
-    async loadComments () {
-      this.loading = true
-      const res = await fetch('https://jsonplaceholder.typicode.com/comments?_limit=4')
-      this.comments = await res.json()
-      this.loading = false
+    loadComments () {
+      fetch('https://jsonplaceholder.typicode.com/comments?_limit=4')
+        .then(async response => {
+          const data = await response.json()
+
+          // check for error response
+          if (!response.ok) {
+            // get error message from body or default to response statusText
+            const error =
+              (data && data.message) || response.statusText || response.status
+
+            return Promise.reject(error)
+          }
+
+          this.loading = true
+          this.comments = data
+          this.loading = false
+        })
+        .catch(error => {
+          this.commentsError = true
+          this.textError.push('Ответ сервера: ' + error)
+        })
     },
     addBlock (block) {
       this.blocks.push(block)
+    },
+    closeCommentsError () {
+      this.commentsError = !this.commentsError
+      this.textError.length = 0
     }
   },
   components: {
-    ResumeForm, ResumeView, ResumeComments, AppLoader
+    ResumeForm,
+    ResumeView,
+    ResumeComments,
+    AppLoader,
+    ErrorComments
   }
 }
 </script>
---
+++ src/components/ErrorComments.vue
@@ -0,0 +1,16 @@
+/* eslint-disable quotes */
+<template>
+  <div class="card">
+    <h4>Комментарии временно не достуны</h4>
+    <p class="class-item">{{ textError1[0] }}</p>
+    <div class="btn-close" @click="$emit('close-CommentsError')"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['textError1']
+}
+</script>
+
+<style></style>
